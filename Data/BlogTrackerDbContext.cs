4d9aeebd3a6296b880a2be19b2024eace2c07750
unknown <vasanthgamer2000@gmail.com>
2023-09-25 15:12:16 +0530

    First Commit

---
+++ Data/BlogTrackerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogTracker.Models;
+
+namespace BlogTracker.Data
+{
+    public class BlogTrackerDbContext : DbContext
+    {
+        public BlogTrackerDbContext (DbContextOptions<BlogTrackerDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlogTracker.Models.BlogInfo> BlogInfo { get; set; } = default!;
+
+        public DbSet<BlogTracker.Models.EmpInfo>? EmpInfo { get; set; }
+        public DbSet<BlogTracker.Models.AdminInfo> AdminInfo { get; set; }
+    }
+}
